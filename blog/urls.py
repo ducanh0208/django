fefb018b61290c53fc903dc50307cac31aa66527
Duc Anh <dangzic679@email.com>
2024-07-21 17:40:32 +0700

    Show post

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.list),
+   path('', views.list, name='blog'),
+   path('<int:id>/', views.post, name='post'),
 ]
-
-urlpatterns = [
-   path('', views.list),
-   path('<int:id>/', views.post),
-]
-
