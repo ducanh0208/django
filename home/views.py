27a65306469d5996825d1dcd762b3492d0d1fd6d
Duc Anh <dangzic679@email.com>
2024-08-18 19:01:12 +0700

    Register home

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .form import RegistrationForm
+
 # Create your views here.
 def index(request):
    return render(request, 'pages/home.html')
@@ -7,3 +9,12 @@ def contact(request):
    return render(request, 'pages/contact.html')
 def error(request, exception):
     return render(request, 'pages/error.html', {'message': exception})
+def register(request):
+    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    return render(request, 'pages/register.html', {'form': form})
+
