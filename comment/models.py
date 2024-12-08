5cbfa858483dc96dcd7829a7014877703b787011
ducanh <dangzic679@gmail.com>
2024-12-08 16:37:03 +0700

    Update db

--- comment/models.py
+++ comment/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Comment(models.Model):
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(
+        'users.CustomUser',
+        on_delete=models.CASCADE,
+    )
     name = models.CharField(max_length=100, blank=True, null=True)
     email = models.EmailField(max_length=255, blank=True, null=True) 
     text = models.TextField() 
     created_at = models.DateTimeField(auto_now_add=True) 
-
+    post = models.ForeignKey(
+        'blog.Post',
+        on_delete=models.CASCADE,
+    )
+    
     def __str__(self):
         return f"Comment by {self.name or 'Anonymous'} on {self.created_at}"
